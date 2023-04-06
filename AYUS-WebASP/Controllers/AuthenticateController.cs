6a3f2364d2c1cae906f8819cff4ce7f18104cbcd
JayMar921 <jayharronabejar@gmail.com>
2023-04-06 09:03:30 +0800

    Added LoginUI

---
+++ AYUS-WebASP/Controllers/AuthenticateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AYUS_WebASP.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
